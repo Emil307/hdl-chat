4cd7db8804307a53cabb4a4c8cc712f350237258
unknown <emilnovikov392@gmail.com>
2023-09-14 16:32:28 +0600

    minor connect global styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes } from 'react-router-dom';
 import { PublicRoutes } from './navigation/PublicRoutes';
+import './styles/index.scss';
 
 const App: React.FC = () => {
   return (
